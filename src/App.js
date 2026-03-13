d07c7bc101727293831725908ec625489075e8d7
MuhammadUmair0412 <mu1640160@gmail.com>
2026-03-13 15:25:55 +0500

    Umair Work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Harder from './components/Harder';
 import Shahzaib from './components/Shahzaib';
+import Umair from './components/Umair';
 
 function App() {
   return (
