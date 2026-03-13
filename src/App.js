b16e4e3889286730c95885f7644b06dfaf6bfaac
shahzaib <shahzaibdon43210@gmail.com>
2026-03-13 15:12:00 +0500

    shahzaib file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Harder from './components/Harder';
+import Shahzaib from './components/Shahzaib';
 
 function App() {
   return (
     <div className="App">
 <Harder/>
+<Shahzaib/>
     </div>
   );
 }
---
+++ src/components/Shahzaib.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shahzaib = () => {
+  return (
+    <div>
+      <h1>User Detalies</h1>
+    </div>
+  )
+}
+
+export default Shahzaib
