bdec1be6f88827d1620ef0b296535cea95ecbe71
shahzaib <shahzaibdon43210@gmail.com>
2026-03-13 15:36:17 +0500

    sssss

--- src/components/Shahzaib.jsx
+++ src/components/Shahzaib.jsx
@@ -3,7 +3,41 @@ import React from 'react'
 const Shahzaib = () => {
   return (
     <div>
-      <h1>User Detalies</h1>
+        <div style={{display:"flex", justifyContent:"space-around"}}>
+      <h1>Shahzaib ali</h1>
+        <div class="input-group flex-nowrap" style={{width:"250px"}}>
+  <input type="text" class="form-control" placeholder="Username"/>
+</div>
+<button type="button" class="btn btn-warning">Warning</button>
+</div>
+<div><h1>Shahzaib and Umair and Gulfam</h1></div>
+      <div>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">FirstName</th>
+      <th scope="col">LastFirst</th>
+      <th scope="col">age</th>
+      <th scope="col">adress</th>
+      <th scope="col">Email</th>
+        <th scope="col">moblie</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  
+  
+  </tbody>
+</table>
+</div>
+
     </div>
   )
 }
