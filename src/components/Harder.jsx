e3a0f7c7d6892be9028cc09bc654c8f3a20b2969
Rana gulfam <gulfamelahi7@gmail.com>
2026-03-13 15:00:37 +0500

    abcd

--- src/components/Harder.jsx
+++ src/components/Harder.jsx
@@ -4,6 +4,20 @@ const Harder = () => {
   return (
     <div>
         <h1>rana gulfam elahi</h1>
+        <div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis optio eligendi aperiam sunt officiis distinctio nam? Nostrum numquam at corporis!</h1>
+<button type='button'>click me</button>
+
+
     </div>
   )
 }
