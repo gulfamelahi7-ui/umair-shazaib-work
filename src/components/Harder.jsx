e7b6e36cae8ed52395e5195ea25f31105a20a062
Rana gulfam <gulfamelahi7@gmail.com>
2026-03-13 15:38:30 +0500

    send git

--- src/components/Harder.jsx
+++ src/components/Harder.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { MdAutoDelete } from "react-icons/md";
+import { Bs3SquareFill } from "react-icons/bs";
 
 const Harder = () => {
   return (
-    <div>
-        <h1>rana gulfam elahi</h1>
-        <div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+    <div>        
+              <div className='hader'>
+      <div className='orange'>
+<h1 style={{color:"blue"}}>User Detial</h1>
 </div>
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis optio eligendi aperiam sunt officiis distinctio nam? Nostrum numquam at corporis!</h1>
+     
+      <div class="input-group mb-3" style={{width:'300px',display:'flex',justifyContent:'center',alignItems:'center'}}>
+  Search:
+  <input type="text" class="form-control" placeholder="Username" />
+</div>
+      <div>
+      {/* <button type='text' placeholder="Search here..." style={{backgroundColor:"blue",color:"white"}}>Create New User</button> */}
+      <button type="button" class="btn btn-primary" >Create new user</button>
+
+      </div>
+
+</div>
+<h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi eligendi maiores doloribus ullam perferendis iste odio quae recusandae saepe amet?</h1>
 <button type='button'>click me</button>
 
+<footer>
+<MdAutoDelete style={{fontSize:'60px'}} />
+<Bs3SquareFill  style={{fontSize:'60px'}} />
+</footer>
 
     </div>
   )
